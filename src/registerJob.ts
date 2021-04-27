be0fa063355d424c24254e316c6b1e2d5c78560c
Phablo Raylan <phabloraylan@gmail.com>
2021-04-27 14:55:09 -0300

    apenas um test

--- src/registerJob.ts
+++ src/registerJob.ts
@@ -1,9 +1,7 @@
 import { TestJob } from '@jobs/TestJob';
-import { Test2Job } from '@jobs/Test2Job';
 import { DoJobService } from '@shared/services/DoJobService';
 import { container } from 'tsyringe';
 
 export default () => {
   container.resolve(DoJobService).execute(new TestJob());
-  container.resolve(DoJobService).execute(new Test2Job());
 };
